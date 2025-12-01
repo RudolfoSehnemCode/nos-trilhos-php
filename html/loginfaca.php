4fac8bb03686719eec150b2e58533fb848097144
Rudolfosehnem <rudolfo_silva@estudante.sesisenai.org.br>
2025-12-01 07:39:40 -0300

    update-finalizacao

--- html/loginfaca.php
+++ html/loginfaca.php
@@ -73,7 +73,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
             <div class="Remenber-me">
                 <input type="checkbox">
-                <p class="box1">Lembrar-me</p>
+                
             </div>
 
             <div class="btn">
