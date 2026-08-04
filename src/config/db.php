5b7a6678061e56226180c65901ece66cff0830d6
RudolfoSehnemCode <rudolfosehnem@gmail.com>
2026-08-04 11:30:42 -0300

    ajuste conexao com db

--- src/config/db.php
+++ src/config/db.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "root"; 
+$password = ""; 
 $dbname = "nos_trilhos";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
