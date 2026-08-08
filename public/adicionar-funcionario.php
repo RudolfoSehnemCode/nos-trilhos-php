4491ccdcf9907d3b08cb8f7d4aa8037cbc016f6d
RudolfoSehnemCode <rudolfosehnem@gmail.com>
2026-08-07 23:53:15 -0300

    wip: unificando sidebar com include, investigando cor/posicao diferente

--- public/adicionar-funcionario.php
+++ public/adicionar-funcionario.php
@@ -145,29 +145,7 @@ if(!isset($_SESSION['id'])){
 </head>
 
 <body>
-  
-    <button id="botaoAbrirSidebar" class="abrir-btn1" onclick="abrirSidebar()">☰</button>
-
-  
-   <div id="sidebar1" class="sidebar1">
-    <p class="menuside1"><strong>Menu</strong></p>
-    <hr>
-    <a href="javascript:void(0)" class="fechar-btn1" onclick="fecharSidebar()">✖</a>
-    <a href="dashboard.php"> <img src="images/casa (1).png" alt=""> Início</a>
-    <a href="gestao.php"> <img src="images/rotas.png" alt="">Gestão de Rotas</a>
-    <a href="manutencao.php"><img src="images/manutencao (2).png" alt="">Manutenção</a>
-    <a href="relatorio.php"> <img src="images/relatorio.png" alt="">Relatório e Análise</a>
-    <a href="alertas.php"><img src="images/sinos.png" alt="">Notificações</a>
-    <a href="dashboard2.php"><img src="images/painel.png" alt=""> Dashboard</a>
-
-    
-
-  <a href="logout.php"> <img src="images/sair (1).png" alt=""> Sair </a>
-  
-  </div>
-  </div>
-
-  
+  <?php include __DIR__ . '/../src/partials/sidebar.php'; ?>  
   <header class="topo2">
 
     <div class="topo2">
@@ -230,28 +208,7 @@ if(!isset($_SESSION['id'])){
 
   <footer>
     <p class="rodape">NOS TRILHOS</p>
-  </footer>
-
-
-<script>
-    function abrirSidebar() {
-      document.getElementById("sidebar1").style.width = "220px";
-      document.getElementById("botaoAbrirSidebar").style.display = "none";
-    }
-
-    function fecharSidebar() {
-      document.getElementById("sidebar1").style.width = "0";
-      document.getElementById("botaoAbrirSidebar").style.display = "block";
-    }
-
-    function mostrarMensagem() {
-      document.getElementById("mensagem").style.display = "block";
-    }
-
-  
-  </script>
-
-  
+  </footer>  
 </body>
 
 </html>
--- public/relatorio.php
+++ public/relatorio.php
@@ -14,31 +14,12 @@ if(!isset($_SESSION['id'])){
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Relatório e análise</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-  
-    <button id="botaoAbrirSidebar" class="abrir-btn1" onclick="abrirSidebar()">☰</button>
-
-  
-   <div id="sidebar1" class="sidebar1">
-    <p class="menuside1"><strong>Menu</strong></p>
-    <hr>
-    <a href="javascript:void(0)" class="fechar-btn1" onclick="fecharSidebar()">✖</a>
-    <a href="dashboard.php"> <img src="images/casa (1).png" alt=""> Início</a>
-    <a href="gestao.php"> <img src="images/rotas.png" alt="">Gestão de Rotas</a>
-    <a href="manutencao.php"><img src="images/manutencao (2).png" alt="">Manutenção</a>
-    <a href="relatorio.php"> <img src="images/relatorio.png" alt="">Relatório e Análise</a>
-    <a href="alertas.php"><img src="images/sinos.png" alt="">Notificações</a>
-    <a href="dashboard2.php"><img src="images/painel.png" alt=""> Dashboard</a>
-  <a href="logout.php"> <img src="images/sair (1).png" alt=""> Sair </a>
-  
-  </div>
-  </div>
-
-  
+  <?php include __DIR__ . '/../src/partials/sidebar.php'; ?>
   <header class="topo2">
     <div class="topo2">
       NOS TRILHOS
@@ -83,34 +64,6 @@ if(!isset($_SESSION['id'])){
       <img src="images/porcent.png" alt="">
     </div>
   </div>
-
-  
-  <script>
-    function abrirSidebar() {
-      document.getElementById("sidebar1").style.width = "220px";
-      document.getElementById("botaoAbrirSidebar").style.display = "none";
-    }
-
-    function fecharSidebar() {
-      document.getElementById("sidebar1").style.width = "0";
-      document.getElementById("botaoAbrirSidebar").style.display = "block";
-    }
-
-    
-    function abrirModalSaida() {
-      document.getElementById("modalSaida").style.display = "flex";
-    }
-
-    function fecharModalSaida() {
-      document.getElementById("modalSaida").style.display = "none";
-    }
-
-    function confirmarSaida() {
-      alert("Você saiu!");
-      fecharModalSaida();
-    }
-  </script>
-
 </body>
 
 </html>
